547a8e6167aa30fc70e2ceb246c19d4218247edf
Fabian Anguiano <fabiananguiano@gmail.com>
2019-06-30 22:53:53 -0500

    Working on styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Anker Reviews`,
     author: `Fabian`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
